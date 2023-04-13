3e3cfaf578dd50c187e542890c008418f48b38a3
John Layher <johnevanlayher@gmail.com>
2023-04-12 23:37:45 -0400

    login component functionality

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+  username = "";
+  password = "";
+  errorMsg = "";
+
+  ngOnInit(): void {
+  }
+
+  login() {
+    if (this.username.trim().length === 0) {
+      this.errorMsg = "Username is required";
+    } else if (this.password.trim().length === 0) {
+     this.errorMsg = "Password is required";
+    }
+  }
 }
